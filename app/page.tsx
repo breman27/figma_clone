3eb0d34ded14c4a1b2d0fb3258d2eddadfdf468c
Brett <brettdev27@gmail.com>
2024-03-02 21:00:28 -0500

    Liveblocks fully implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import Live from "@/components/Live";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div>
-      <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="h-full flex flex-row">
+        <Live />
+      </section>
+    </main>
   );
 }
