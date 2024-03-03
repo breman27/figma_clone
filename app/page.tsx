9ea74a44beb60f4e975dd18feb09a8f1f290ea08
Brett <brettdev27@gmail.com>
2024-03-03 08:29:33 -0500

    Add new components for drop down menus

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,55 @@
 "use client";
 
+import { fabric } from "fabric";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import React, { useEffect, useRef } from "react";
+import {
+  handleCanvasMouseDown,
+  handleResize,
+  initializeFabric,
+} from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
 
 export default function Home() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef<fabric.Canvas | null>(null);
+  const isDrawing = useRef(false);
+  const shapeRef = useRef<fabric.Object | null>(null);
+  const selectedShapeRef = useRef<string | null>("rectangle");
+  const [activeElement, setActiveElement] = React.useState<ActiveElement>({
+    name: "",
+    value: "",
+    icon: "",
+  });
+
+  useEffect(() => {
+    const canvas = initializeFabric({ canvasRef, fabricRef });
+
+    canvas.on("mouse:down", (options: any) => {
+      handleCanvasMouseDown({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+      });
+    });
+
+    window.addEventListener("resize", () => {
+      handleResize({ canvas: fabricRef });
+    });
+  }, []);
+
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar />
+      <Navbar activeElement={activeElement} />
       <section className="h-full flex flex-row">
-        <Live />
+        <LeftSidebar />
+        <Live canvasRef={canvasRef} />
+        <RightSidebar />
       </section>
     </main>
   );
